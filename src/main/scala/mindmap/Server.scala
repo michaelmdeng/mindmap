4079b13ce58bfd6dd681fb9fdba8ffa9b74aee19
Michael Deng <33045922+michaelmdeng@users.noreply.github.com>
2023-04-23 15:30:59 -0700

    feat: add tofu logging and remove old logging (#55)
    * feat: add tofu logging and remove old logging
    
    * feat: update pom for tofu

--- src/main/scala/mindmap/Server.scala
+++ src/main/scala/mindmap/Server.scala
@@ -10,9 +10,10 @@ import org.http4s.implicits._
 import org.http4s.server._
 import org.http4s.server.staticcontent._
 import org.http4s.syntax.all._
-import play.twirl.api._
+import tofu.logging.Logging
+import tofu.syntax.logging._
+import tofu.Delay
 
-import mindmap.effect.Logging
 import mindmap.effect.configuration.RealConfiguration
 import mindmap.effect.controller.NotesController
 import mindmap.effect.controller.TagsController
@@ -21,7 +22,12 @@ import mindmap.effect.zettelkasten.MemoryZettelkastenRepository
 import mindmap.model.configuration.ConfigurationAlgebra
 
 object Server extends IOApp {
-  private implicit val logger: Logging[IO] = new Logging(Server.getClass())
+  private implicit val ioDelay: Delay[IO] = new Delay[IO] {
+    def delay[A](fa: => A): IO[A] = IO.delay(fa)
+  }
+  private implicit val makeLogging: Logging.Make[IO] = Logging.Make.plain[IO]
+  private implicit val log: Logging[IO] =
+    Logging.Make[IO].forService[IOApp]
 
   def createGraphService(blocker: Blocker): HttpRoutes[IO] = {
     fileService[IO](FileService.Config("public/graph", blocker))
@@ -42,12 +48,33 @@ object Server extends IOApp {
         Grapher.graph(config)
       )
       zettelRepo = new MemoryZettelkastenRepository[IO](collection)
-      notes = new NotesController[IO](zettelRepo).routes()
-      tags = new TagsController[IO](zettelRepo).routes()
-      warnings = new WarningController[IO](graphWarnings, repoWarnings).routes()
-      assets = createAssetsService(blocker)
-      graph = createGraphService(blocker)
-      network = createNetworkService(blocker)
+      _ <- Resource.eval(info"initialized zettelkasten service")
+      (notes, tags, warnings) <- (
+        Resource.eval(new NotesController[IO](zettelRepo).routes().pure[IO]) <* Resource
+          .eval(
+            info"initialized notes controller"
+          ),
+        Resource.eval(new TagsController[IO](zettelRepo).routes().pure[IO]) <* Resource
+          .eval(
+            info"initialized tags controller"
+          ),
+        Resource.eval(
+          new WarningController[IO](graphWarnings, repoWarnings)
+            .routes()
+            .pure[IO]
+        ) <* Resource.eval(info"initialized warning controller")
+      ).parMapN((a, b, c) => (a, b, c))
+      (assets, graph, network) <- (
+        Resource.eval(createAssetsService(blocker).pure[IO]) <* Resource.eval(
+          info"initialized assets service"
+        ),
+        Resource.eval(createGraphService(blocker).pure[IO]) <* Resource.eval(
+          info"initialized graph service"
+        ),
+        Resource.eval(createNetworkService(blocker).pure[IO]) <* Resource.eval(
+          info"initialized network service"
+        )
+      ).parMapN((a, b, c) => (a, b, c))
       app = Router(
         "/assets" -> assets,
         "/graph" -> graph,
@@ -74,7 +101,7 @@ object Server extends IOApp {
       }
       config = RealConfiguration[IO](path)
       _ <- createServer(config)
-        .use(_ => logger.info("Initialized server") *> IO.never)
+        .use(_ => info"Initialized server" *> IO.never)
         .as(ExitCode.Success)
     } yield (ExitCode.Success)
   }
